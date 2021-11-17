e63bd471d7a70527960d9567ab8e573daeb7a833
Tomasz Pluskiewicz <588128+undefined@users.noreply.github.com>
2021-11-17 18:24:47 +0000

    material

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import "@hydrofoil/shaperone-wc/shaperone-form"
+import { renderer, components } from '@hydrofoil/shaperone-wc/configure'
+import {  } from '@hydrofoil/shaperone-wc-material'
+import * as MaterialRenderStrategy from '@hydrofoil/shaperone-wc-material/renderer'
+import * as mwcComponents from '@hydrofoil/shaperone-wc-material/components'
 import { html, render } from "lit"
 import { loadShape } from './localShape.js'
 
 const body = document.querySelector('body')
+
+components.pushComponents(mwcComponents)
+renderer.setTemplates(MaterialRenderStrategy)
 
 ;(async function () {
   const shape = await loadShape()
