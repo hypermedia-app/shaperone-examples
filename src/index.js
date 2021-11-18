293f6eed944b721cc2f3169c22fd50a98478737b
Tomasz Pluskiewicz <588128+undefined@users.noreply.github.com>
2021-11-18 18:32:20 +0000

    remove lit dependency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "@hydrofoil/shaperone-wc/shaperone-form.js"
-import { html } from '@hydrofoil/shaperone-wc'
+import { html, render } from '@hydrofoil/shaperone-wc'
 import { renderer, components } from '@hydrofoil/shaperone-wc/configure'
 import * as MaterialRenderStrategy from '@hydrofoil/shaperone-wc-material/renderer'
 import * as mwcComponents from '@hydrofoil/shaperone-wc-material/components'
-import { render } from "lit"
 import { loadShape } from './localShape.js'
 
 const body = document.querySelector('body')
