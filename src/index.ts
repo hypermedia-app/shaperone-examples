c4dd5f1cffc18a923cbe5c7f069591178350e8fb
Tomasz Pluskiewicz <588128+undefined@users.noreply.github.com>
2021-11-19 15:07:01 +0000

    local shape from ttl

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import "@hydrofoil/shaperone-wc/shaperone-form.js"
 import { html, render } from '@hydrofoil/shaperone-wc'
+import { templates } from '@hydrofoil/shaperone-wc/templates.js'
 import { renderer, components } from '@hydrofoil/shaperone-wc/configure'
 import * as MaterialRenderStrategy from '@hydrofoil/shaperone-wc-material/renderer'
-import * as mwcComponents from '@hydrofoil/shaperone-wc-material/components'
-import { loadShape } from './localShape.js'
+import * as mwcComponents from '@hydrofoil/shaperone-wc-material/components.js'
+import { loadShape } from './fetchShape'
 
 const body = document.querySelector('body')
 
 components.pushComponents(mwcComponents)
-renderer.setTemplates(MaterialRenderStrategy)
+renderer.setTemplates({
+  ...MaterialRenderStrategy,
+  focusNode: MaterialRenderStrategy.focusNode(templates.focusNode)
+})
 
 ;(async function () {
-  const shape = await loadShape()
+  const shape = await loadShape({
+    shape: 'person'
+  })
 
   render(html`<shaperone-form .shapes=${shape}></shaperone-form>`, body)
 })()
