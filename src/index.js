deb96c188834be33b949eac4e5eac948f0b4e2e4
Tomasz Pluskiewicz <588128+undefined@users.noreply.github.com>
2021-12-15 20:09:41 +0000

    Push from star-rating

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './config'
 const form = document.querySelector('shaperone-form')
 
 ;(async function () {
-  const shapes = await fetchShapes('person')
+  const shapes = await fetchShapes('purchase')
 
   form.shapes = shapes.namedNode('')
 })()
